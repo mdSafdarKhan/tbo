a6686420c98c92aa7201946d88b03f431fe5d716
MD SAFDAR KHAN <30369076+mdSafdarKhan@users.noreply.github.com>
2020-09-17 11:50:39 +0530

    Add files via upload

---
+++ tbo-place/src/main/java/com/tbo/place/TboPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.tbo.place;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TboPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TboPlaceApplication.class, args);
+	}
+
+}
---
+++ tbo-place/src/test/java/com/tbo/place/TboPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbo.place;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TboPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
